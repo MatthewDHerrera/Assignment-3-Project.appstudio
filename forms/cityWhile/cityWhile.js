f061b19d694a5dfda19ec2a1c64d8209f8372f4e
MatthewDHerrera <matthewdavidherrera@gmail.com>
2021-10-03 21:06:01 -0500

    Initial commit

---
+++ forms/cityWhile/cityWhile.js
@@ -0,0 +1,13 @@
+let userCity = [] 
+let addAnotherCity = "Yes"
+let i = 0 
+
+
+while(addAnotherCity == "Yes") {
+  let nextCity = prompt("What city would you like to add to the list?")
+  userCity.push(nextCity.toLowerCase())
+  addAnotherCity = prompt ("Type \"Yes\" to add another city, otherwise click cancel")
+  }
+  
+
+while 
